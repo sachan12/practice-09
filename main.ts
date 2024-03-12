9a00dc72a188d0cd78b5dee699a27c96ccfc85d9
sachan12 <sachinnanda427@gmail.com>
2024-03-12 13:56:52 -0700

    add these files

---
+++ main.ts
@@ -0,0 +1,103 @@
+// LOGICAL OPERATORS | CONDITIONAL STATEMENTS
+// Logical Operators (1-And (&&) 2-or(||) 3-not(!));
+// let x =10
+// console.log(x===10);         // true
+// And(&&)
+// console.log(true && true);   // true
+// console.log(true && false);  // false
+// console.log(false && true);  // false
+// console.log(false && false); // false
+// let x = 10
+// let y = 20
+// console.log(x===10 && y===20);  //True
+// console.log(x===10 && y===10);  // False
+// console.log(x<15 && y>15);      // True
+
+
+// or(||)
+// let a = 10;
+// let b = 20;
+
+// console.log(true || true);  //True
+// console.log(true || false); // True
+// console.log(false || true); // True
+// console.log(false || false); // false
+
+// console.log(a===10 || b===12);  //True
+// console.log(a===10 || b===20);  //True
+// console.log(a===5 || b===20);   //True
+// console.log(a===2 || b===3);    //false
+
+//not(!)
+// console.log(true===true);   // true
+// console.log(!true === true); // false
+// console.log (!true === false); // true
+// console.log(false===false);    // true
+// console.log(a!== 10); // false
+
+// ======================= Conditional Statements (if , elseif , else);======================
+
+// let personAge = 20
+// if(personAge > 18){
+//     console.log("He can vote");
+// }else{
+//     console.log('You can not vote');
+// }
+
+// let temperature = 12;
+// if (temperature>40){
+//   console.log('The weather is very hot , Better stay at home');
+// }else if (temperature > 30){
+// console.log("The Weather is Hot");
+// }else if (temperature > 10){
+//     console.log("The weather is cold"); 
+// }
+// else{
+//     console.log('The weather is very cold');
+// }
+// let ram = '8gb';
+// let generation = '6th';
+// let storage = '128gb';
+// if (ram==='8gb'&& generation==='6th' && storage==='256'){
+//     console.log("I can buy this laptop");
+// } else {
+//     console.log('I can not buy this laptop'); 
+// }
+
+// if (ram==='8gb'&& generation==='6th' || storage==='256'){
+//     console.log("I can buy this laptop");
+// } else {
+//     console.log('I can not buy this laptop'); 
+// }
+
+// if (ram==='16gb'|| generation==='8th' || storage==='256'){
+//     console.log("I can buy this laptop");
+// } else {
+//     console.log('I can not buy this laptop'); 
+// }
+// let ram = 10;
+// let generation = 6;
+// let storage = 128;
+
+// if(ram>4 && ram<=8){
+//     console.log('I like this laptop');
+// }else{
+//     console.log("I can not buy this laptop");  
+// }
+
+ // =============Governer Assigment Question = 25
+//  let alienColor = 'green';
+//  if(alienColor==='green'){
+//     console.log("The player just earned 5 points");
+//  }
+//  if(alienColor==='red'){
+//     console.log("The version that fais will have no output");  
+//  }
+
+
+
+
+
+
+
+
